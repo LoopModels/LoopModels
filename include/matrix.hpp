ec395930b13ac5cde43d4bfb69971a3c71a53ec1
Yingbo Ma <mayingbo5@gmail.com>
2021-11-05 12:37:32 -0400

    Basic stuffs

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -1,4 +1,5 @@
 #include <cstddef>
+#include <cstdio>
 #include <array>
 
 template <class T, size_t M, size_t N>
@@ -9,4 +10,25 @@ struct Matrix {
     const std::array<size_t, D> dims;
 
     Matrix(T* content, const std::array<size_t, D> dims) : content(content), dims(dims) { };
+
+    size_t getSize(size_t i) {
+        if (i == 0) {
+            return (M != 0) ? M : dims[0];
+        } else {
+            return (N != 0) ? N : dims[D-1];
+        }
+    }
+
+    T& operator()(size_t i, size_t j) {
+        return content[i + j * getSize(0)];
+    }
+
+    void show() {
+        for (size_t i = 0; i < getSize(0); i++) {
+            for (size_t j = 0; j < getSize(1); j++) {
+                std::printf("%17d", (*this)(i, j));
+            }
+            std::printf("\n");
+        }
+    }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,12 @@
 
 int main() {
     const std::array<size_t, 2> dims{ {2, 2} };
-    int* f = nullptr;
+    int* f = new int[4];
+    for (size_t i=0; i<4; i++) f[i] = i + 1;
     Matrix<int, 0, 0> M(f, dims);
     fmt::print("dims: {}, {}\n",  M.dims[0], M.dims[1]);
+    fmt::print("A[0, 1]: {}\n",  M(0, 1));
+    M.show();
+    delete[] f;
     return 0;
 }
