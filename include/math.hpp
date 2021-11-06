e6494aebffd9ac52906072417988812e165a3cce
Yingbo Ma <mayingbo5@gmail.com>
2021-11-06 02:18:36 -0400

    Going to bed
    Co-authored-by: "Chris Elrod" <elrodc@gmail.com>

--- include/math.hpp
+++ include/math.hpp
@@ -7,6 +7,26 @@ const size_t MAX_NUM_LOOPS = 16;
 const size_t MAX_PROGRAM_VARIABLES = 32;
 typedef int32_t Int;
 
+template <class T, size_t M> struct Vector {
+    static constexpr size_t D = !M;
+
+    T* ptr;
+    const std::array<size_t, D> dims;
+
+    Vector(T *ptr, const std::array<size_t, D> dims) : ptr(ptr), dims(dims) {};
+
+    size_t getSize() {return (M == 0) ? dims[0] : M;}
+
+    T &operator()(size_t i) { return ptr[i]; }
+
+    void show() {
+        for (size_t i = 0; i < getSize(); i++) {
+            std::printf("%17d", (*this)(i));
+        }
+        std::printf("\n");
+    }
+};
+
 template <class T, size_t M, size_t N> struct Matrix {
     static constexpr size_t D = (!M + !N);
 
@@ -33,19 +53,17 @@ template <class T, size_t M, size_t N> struct Matrix {
             std::printf("\n");
         }
     }
+
+    Vector<T, M> getCol(size_t i) {
+        if (M == 0) {
+            return Vector<T, M>(ptr + i * getSize(0), std::array<size_t, 1>{{getSize(0)}});
+        } else {
+            return Vector<T, M>(ptr + i * getSize(0), std::array<size_t, 0>{{}});
+        }
+    }
 };
 
 template <typename T> size_t getNLoops(T x) { return x.data.dims[0]; }
-
-struct RectangularLoopNest {
-    typedef Matrix<Int, MAX_PROGRAM_VARIABLES, 0> M;
-    M data;
-
-    RectangularLoopNest(Int *ptr, size_t nloops)
-        : data(M(ptr, std::array<size_t, 1>{{nloops}})) {
-        assert(nloops <= MAX_NUM_LOOPS);
-    };
-};
 
 struct Permutation {
     typedef Matrix<Int, 0, 2> M;
@@ -137,7 +155,284 @@ std::pair<PermutationSubset, bool> advance_state(PermutationLevelIterator p,
     }
 }
 
-// bool compatible(RectangularLoopNest l1, RectangularLoopNest l2, Permutation
-// perm1, Permutation perm2, int32_t i1, int32_t i2){
+typedef Matrix<Int, MAX_PROGRAM_VARIABLES, 0> RektM;
 
-// }
+struct RectangularLoopNest {
+    RektM data;
+
+    RectangularLoopNest(Int *ptr, size_t nloops)
+        : data(RektM(ptr, std::array<size_t, 1>{{nloops}})) {
+        assert(nloops <= MAX_NUM_LOOPS);
+    };
+};
+
+/*
+function compatible(l1::TriangularLoopNest, l2::RectangularLoopNest, perm1::Permutation, perm2::Permutation, _i1::Int, _i2::Int = _i1)
+  @unpack A, r = l1
+  i = perm1[_i1]
+  ub1 = getupperbound(r, i)
+  ub2 = getupperbound(l2, perm2[_i2])
+  Δb = SBVector{32,Int32}(undef)
+  for j ∈ eachindex(Δb)
+    Δb[j] = ub1[j] - ub2[j]
+  end
+  # now need to add `A`'s contribution
+  iperm = inv(perm1)
+  # the first loop adds variables that adjust `i`'s bounds
+  for j in SafeCloseOpen(i)
+    # Aᵢⱼ = A[i,j]
+    Aᵢⱼ = A[j,i] # upper half is reflected
+    Aᵢⱼ == zero(Aᵢⱼ) && continue
+    j1 = iperm[j]
+    j1 < _i1 && return false # j1 < _i1 means it is included in the permutation, but rectangular `l2` definitely does not depend on `j` loop!
+    # we have i < C - Aᵢⱼ * j
+    if Aᵢⱼ < zero(Aᵢⱼ) # i < C + j*abs(Aᵢⱼ)
+      # mx, independent = independent_maximum(A, j, perm)
+      # independent || return false
+      # TODO: relax restriction
+      otherwise_independent(A, j, i) || return false
+      ub_temp = getupperbound(r, j)
+      for k  eachindex(Δb)
+        Δb[k] -= Aᵢⱼ * ub_temp[k]
+      end
+      Δb[0] += Aᵢⱼ
+    else#if Aᵢⱼ > zero(Aᵢⱼ) # i < C - j*abs(Aᵢⱼ)
+      # Aᵢⱼ > zero(Aᵢⱼ) means that `j_lower_bounded_by_k` will be false when `k=i`.
+      zero_minimum(A, j, j1, perm) || return false
+      # otherwise_independent(A, j, i) || return false
+    end
+  end
+  # the second loop here defines additional bounds on `i`
+  # If `j` below is in the permutation, we can rule out compatibility with rectangular `l2` loop
+  # If it is not in the permutation, then the bound defined by the first loop holds, so no checks/adjustments needed here.
+  for j ∈ SafeCloseOpen(i+1, size(A,1))
+    Aᵢⱼ = A[j,i]
+    Aᵢⱼ == zero(Aᵢⱼ) && continue
+    iperm[j] < _i1 && return false # j1 < _i1 means it is included in the permutation, but rectangular `l2` definitely does not depend on `j` loop!
+  end
+  iszero(Δb[0]) && return all(iszero, Δb)
+  ((Δb[0] == Int32(-1)) && all(iszero, Δb[1:end])) || return false
+  return zero_inner_iterations_at_maximum(A, ub2, r, i)
+end
+compatible(r::RectangularLoopNest, t::TriangularLoopNest, perm2::Permutation, perm1::Permutation, _i2, _i1 = _i2) = compatible(t, r, perm1, perm2, _i1, _i2)
+
+# inlined, because we want Δb to be stack allocated.
+# maybe just GC preserve the memory instead?
+@inline function update_bound_difference!(Δb, l1, A2, perm1, perm2, _i1, i2, flip)
+  @unpack A1, r1 = l1
+  i1 = perm1[_i1]
+  iperm = inv(perm1)
+  # the first loop adds variables that adjust `i`'s bounds
+  for j in SafeCloseOpen(i1)
+    # Aᵢⱼ = A[i,j]
+    Aᵢⱼ = A1[j,i1] # upper half is reflected
+    Aᵢⱼ == zero(Aᵢⱼ) && continue
+    j1 = iperm[j]
+    if j1 < _i1
+      # j1 < _i1 means it is included in the permutation
+      # must check to confirm that A2[j,i] equals
+      A2[perm2[j1],i2] ≠ Aᵢⱼ && return false
+    end
+    # we have i < C - Aᵢⱼ * j
+    if Aᵢⱼ < zero(Aᵢⱼ) # i < C + j*abs(Aᵢⱼ)
+      # mx, independent = independent_maximum(A, j, perm)
+      # independent || return false
+      # TODO: relax restriction
+      otherwise_independent(A1, j, i1) || return false
+      ub_temp = getupperbound(r1, j)
+      Aᵢⱼ = ifelse(flip, -Aᵢⱼ, Aᵢⱼ)
+      for k ∈ eachindex(Δb)
+        Δb[k] -= Aᵢⱼ * ub_temp[k]
+      end
+      Δb[0] += Aᵢⱼ
+    else#if Aᵢⱼ > zero(Aᵢⱼ) # i < C - j*abs(Aᵢⱼ)
+      # Aᵢⱼ > zero(Aᵢⱼ) means that `j_lower_bounded_by_k` will be false when `k=i`.
+      zero_minimum(A1, j, j1, perm1) || return false
+      # otherwise_independent(A, j, i) || return false
+    end
+  end
+  true
+end
+@inline function check_remaining_bound(l1, A2, perm1, perm2, _i1, i2)
+  @unpack A1, r1 = l1
+  i1 = perm1[_i1]
+  iperm = inv(perm1)
+  # the second loop here defines additional bounds on `i`
+  # If `j` below is in the permutation, we can rule out compatibility with rectangular `l2` loop
+  # If it is not in the permutation, then the bound defined by the first loop holds, so no checks/adjustments needed here.
+  for j ∈ SafeCloseOpen(i1+1, size(A1,1))
+    Aᵢⱼ = A1[j,i1]
+    Aᵢⱼ == zero(Aᵢⱼ) && continue
+    j1 = iperm[j]
+    if j1 < _i1
+      # j1 < _i1 means it is included in the permutation
+      # must check to confirm that A2[j,i] equals
+      A2[perm2[j1],i2] ≠ Aᵢⱼ && return false
+    end
+  end
+  return true
+end
+
+function compatible(l1::TriangularLoopNest, l2::TriangularLoopNest, perm1::Permutation, perm2::Permutation, _i1::Int, _i2::Int = _i1)
+  @unpack A1, r1 = l1
+  @unpack A2, r2 = l2
+  i1 = perm1[_i1]
+  i2 = perm2[_i2]
+  ub1 = getupperbound(r1, i1)
+  ub2 = getupperbound(r2, i2)
+  Δb = SBVector{32,Int32}(undef)
+  for j ∈ eachindex(Δb)
+    Δb[j] = ub1[j] - ub2[j]
+  end
+  # now need to add `A`'s contribution
+  update_bound_difference!(Δb, l1, A2, perm1, perm2, _i1, i2, false) || return false
+  update_bound_difference!(Δb, l2, A1, perm2, perm1, _i2, i1, true)  || return false
+
+  check_remaining_bound(l1, A2, perm1, perm2, _i1, i2) || return false
+  check_remaining_bound(l2, A1, perm2, perm1, _i2, i1) || return false
+
+  Δb₀ = Δb[0]
+  if iszero(Δb₀)
+    return all(iszero, Δb)
+  elseif Δb₀ == Int32(-1)
+    return all(iszero, Δb[1:end]) && zero_inner_iterations_at_maximum(A1, ub2, r1, i1)
+  elseif Δb₀ == Int32(1)
+    return all(iszero, Δb[1:end]) && zero_inner_iterations_at_maximum(A2, ub1, r2, i2)
+  else
+    return false
+  end
+end
+*/
+
+bool compatible(RectangularLoopNest l1, RectangularLoopNest l2, Permutation perm1, Permutation perm2, Int i1, Int i2){
+    i1 = perm1(i1, 0);
+    i2 = perm2(i2, 0);
+    for (auto i=0; i < MAX_PROGRAM_VARIABLES; i++) {
+        if (l1.data(i, i1) != l2.data(i, i2)) return false;
+    }
+    return true;
+}
+
+typedef Matrix<Int, 0, 0> TrictM;
+
+struct TriangularLoopNest {
+    Int *raw;
+    size_t nloops;
+
+    TriangularLoopNest(Int *ptr, size_t nloops) : raw(ptr), nloops(nloops) {
+        assert(nloops <= MAX_NUM_LOOPS);
+    };
+
+    RectangularLoopNest getRekt() {
+        return RectangularLoopNest(raw, nloops);
+    }
+
+    TrictM getTrit() {
+        TrictM A(raw + nloops * MAX_NUM_LOOPS, std::array<size_t, 2>{{nloops,nloops}});
+        return A;
+    }
+};
+
+bool otherwiseIndependent(TrictM A, Int j, Int i) {
+    for (auto k = 0; k < j; k++)
+        if (!A(k, j)) return false; // A is symmetric
+    for (auto k = j+1; k < A.getSize(0); k++)
+        if (!((k == i) | (A(k, j) == 0))) return false;
+    return true;
+}
+
+bool zeroMinimum(TrictM A, Int j, Int _j, Permutation perm) {
+    for (auto k = j+1; k < A.getSize(0); k++) {
+        auto j_lower_bounded_by_k = A(k, j) < 0;
+        if (!j_lower_bounded_by_k) continue;
+        auto _k = perm(k, 1);
+        // A[k,j] < 0 means that `k < C + j`, i.e. `j` has a lower bound of `k`
+        auto k_in_perm = _k < _j;
+        if (k_in_perm) return false;
+        // if `k` not in perm, then if `k` has a zero minimum
+        // `k` > j`, so it will skip
+        if (!zeroMinimum(A, k, _k, perm)) return false;
+    }
+    return true;
+}
+
+bool upperboundDominates(RektM ubi, Int i, RektM ubj, Int j) {
+    bool all_le = true;
+    for (auto k = 0; k < MAX_PROGRAM_VARIABLES; k++){
+        all_le &= (ubi(k,i) >= ubj(k,j));
+    }
+    return all_le;
+}
+
+bool zeroInnerIterationsAtMaximum(TrictM A, RektM ub, RectangularLoopNest r, Int i) {
+    for (auto j = 0; j < i; j++) {
+        auto Aij = A(i, j);
+        if (Aij >= 0) continue;
+        if (upperboundDominates(ub, i, r.data, j)) return true;
+    }
+    for (auto j = i+1; j < A.getSize(0); j++) {
+        auto Aij = A(i, j);
+        if (Aij <= 0) continue;
+        if (upperboundDominates(ub, i, r.data, j)) return true;
+    }
+    return false;
+}
+
+/*
+
+function compatible(l1::TriangularLoopNest, l2::RectangularLoopNest, perm1::Permutation, perm2::Permutation, _i1::Int, _i2::Int = _i1)
+  @unpack A, r = l1
+  i = perm1[_i1]
+  ub1 = getupperbound(r, i)
+  ub2 = getupperbound(l2, perm2[_i2])
+  Δb = SBVector{32,Int32}(undef)
+  for j ∈ eachindex(Δb)
+    Δb[j] = ub1[j] - ub2[j]
+  end
+  # now need to add `A`'s contribution
+  iperm = inv(perm1)
+  # the first loop adds variables that adjust `i`'s bounds
+  for j in SafeCloseOpen(i)
+    # Aᵢⱼ = A[i,j]
+    Aᵢⱼ = A[j,i] # upper half is reflected
+    Aᵢⱼ == zero(Aᵢⱼ) && continue
+    j1 = iperm[j]
+    j1 < _i1 && return false # j1 < _i1 means it is included in the permutation, but rectangular `l2` definitely does not depend on `j` loop!
+    # we have i < C - Aᵢⱼ * j
+    if Aᵢⱼ < zero(Aᵢⱼ) # i < C + j*abs(Aᵢⱼ)
+      # mx, independent = independent_maximum(A, j, perm)
+      # independent || return false
+      # TODO: relax restriction
+      otherwise_independent(A, j, i) || return false
+      ub_temp = getupperbound(r, j)
+      for k  eachindex(Δb)
+        Δb[k] -= Aᵢⱼ * ub_temp[k]
+      end
+      Δb[0] += Aᵢⱼ
+    else#if Aᵢⱼ > zero(Aᵢⱼ) # i < C - j*abs(Aᵢⱼ)
+      # Aᵢⱼ > zero(Aᵢⱼ) means that `j_lower_bounded_by_k` will be false when `k=i`.
+      zero_minimum(A, j, j1, perm) || return false
+      # otherwise_independent(A, j, i) || return false
+    end
+  end
+  # the second loop here defines additional bounds on `i`
+  # If `j` below is in the permutation, we can rule out compatibility with rectangular `l2` loop
+  # If it is not in the permutation, then the bound defined by the first loop holds, so no checks/adjustments needed here.
+  for j ∈ SafeCloseOpen(i+1, size(A,1))
+    Aᵢⱼ = A[j,i]
+    Aᵢⱼ == zero(Aᵢⱼ) && continue
+    iperm[j] < _i1 && return false # j1 < _i1 means it is included in the permutation, but rectangular `l2` definitely does not depend on `j` loop!
+  end
+  iszero(Δb[0]) && return all(iszero, Δb)
+  ((Δb[0] == Int32(-1)) && all(iszero, Δb[1:end])) || return false
+  return zero_inner_iterations_at_maximum(A, ub2, r, i)
+end
+compatible(r::RectangularLoopNest, t::TriangularLoopNest, perm2::Permutation, perm1::Permutation, _i2, _i1 = _i2) = compatible(t, r, perm1, perm2, _i1, _i2)
+
+bool compatible(TriangularLoopNest l1, RectangularLoopNest l2, Permutation perm1, Permutation perm2, Int _i1, Int _i2) {
+    auto A = getTrit(l1);
+    auto r = getRekt(l1);
+    auto i = perm1(_i1, 0);
+    return zeroInnerIterationsAtMaximum(A, ub2, r, i)
+}
+*/
