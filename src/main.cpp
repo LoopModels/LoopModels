699705d253b41edf2d3a9e7f736b4a7d3beba263
Yingbo Ma <mayingbo5@gmail.com>
2022-01-13 14:57:25 -0500

    Make it build on my MacBook

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <cstdio>
 #include <cstddef>
 #include <array>
-#include <fmt/format.h>
 #include "../include/math.hpp"
 #include "../include/show.hpp"
 
@@ -15,8 +14,6 @@ int main() {
             M(n,m) = 10*n+m;
         }
     }
-    fmt::print("dims: {}, {}\n",  size(M, 0), size(M, 1));
-    fmt::print("A[0, 1]: {}\n",  M(0, 1));
     showln(M);
     // delete[] f;
     return 0;
