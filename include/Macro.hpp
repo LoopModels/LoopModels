f2a0733762389ee582e7ace1f12a78646e27b58f
chriselrod <elrodc@gmail.com>
2022-05-30 20:03:43 -0400

    gcc macro expand mess
    Co-authored-by: Yingbo Ma <mayingbo5@gmail.com>

--- include/Macro.hpp
+++ include/Macro.hpp
@@ -1,6 +1,11 @@
 #pragma once
 #if defined(__clang__)
-#define MULTIVERSION __attribute__((target_clones("avx512dq", "avx2", "default")))
+#define MULTIVERSION                                                           \
+    __attribute__((target_clones("avx512dq", "avx2", "default")))
 #else
-#define MULTIVERSION __attribute__((target_clones("arch=skylake-avx512", "avx2", "default")))
+#define MULTIVERSION                                                           \
+    __attribute__((target_clones(                                              \
+        "arch=skylake-avx512,arch=cascadelake,arch=icelake-client,arch=tigerlake,"    \
+        "arch=icelake-server,arch=sapphirerapids,arch=cooperlake",              \
+        "avx2", "default")))
 #endif
