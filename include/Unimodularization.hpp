1b1cf16e453254425e7a178f4f435bbcf4439dbb
Chris Elrod <elrodc@gmail.com>
2022-06-13 00:31:32 -0400

    fix unimodularization_test

--- include/Unimodularization.hpp
+++ include/Unimodularization.hpp
@@ -13,18 +13,19 @@
 
 // if `A` can be unimodularized, returns the inverse of the unimodularized `A`
 llvm::Optional<SquareMatrix<int64_t>> unimodularize(IntMatrix A) {
-    auto OHNF = NormalForm::hermite(std::move(A));
+    llvm::Optional<std::pair<IntMatrix, SquareMatrix<int64_t>>> OHNF =
+        NormalForm::hermite(std::move(A));
     if (!OHNF.hasValue()) {
         return {};
     }
-    auto [H, U] = OHNF.getValue();
-    for (size_t m = 0; m < H.size(0); ++m) {
+    auto &[H, U] = OHNF.getValue();
+    for (size_t m = 0; m < H.numCol(); ++m) {
         if (H(m, m) != 1) {
             // unimodularization was not succesful
             return {};
         }
     }
-    return U;
+    return std::move(U);
 }
 /*
 
