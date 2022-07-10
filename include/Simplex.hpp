5f465f5d57f0169a235e7d1c4b6a151acbf20397
Chris Elrod <elrodc@gmail.com>
2022-07-10 02:40:18 -0400

    some progress; still need to add shifting

--- include/Simplex.hpp
+++ include/Simplex.hpp
@@ -20,49 +20,62 @@ struct Simplex {
     // row 1: cost numerators remaining rows: tableau numerators
     // column 0: indicates whether that row (constraint) is basic,
     //           and if so which one
-    // column 1: denominator for the row
-    // column 2: constraint values
+    // column 1: constraint values
     Matrix<int64_t, 0, 0, 0> tableau;
     size_t numSlackVar;
     bool inCanonicalForm;
+    static constexpr size_t numExtraRows = 2;
+    static constexpr size_t numExtraCols = 1;
+    static constexpr size_t numTableauRows(size_t i) {
+        return i + numExtraRows;
+    }
+    static constexpr size_t numTableauCols(size_t j) {
+        return j + numExtraCols;
+    }
     // NOTE: all methods resizing the tableau may invalidate references to it
     void resize(size_t numCon, size_t numVar) {
-        tableau.resize(numCon + 2, numVar + 2);
+        tableau.resize(numTableauRows(numCon), numTableauCols(numVar));
     }
     void resize(size_t numCon, size_t numVar, size_t stride) {
-        tableau.resize(numCon + 2, numVar + 2, stride);
+        tableau.resize(numTableauRows(numCon), numTableauCols(numVar), stride);
     }
     void addVars(size_t numVars) {
-        tableau.resize(tableau.numRow(), numVars + 2,
-                       std::max(numVars + 2, tableau.rowStride()));
+        size_t numCol = numTableauCols(numVars);
+        tableau.resize(tableau.numRow(), numCol,
+                       std::max(numCol, tableau.rowStride()));
     }
     void truncateVars(size_t numVars) { tableau.truncateCols(numVars + 2); }
     void truncateConstraints(size_t numVars) {
-        tableau.truncateRows(numVars + 2);
+        tableau.truncateRows(numTableauCols(numVars));
     }
     void resizeForOverwrite(size_t numCon, size_t numVar) {
-        tableau.resizeForOverwrite(numCon + 2, numVar + 2);
+        tableau.resizeForOverwrite(numTableauRows(numCon),
+                                   numTableauCols(numVar));
     }
     void resizeForOverwrite(size_t numCon, size_t numVar, size_t stride) {
-        tableau.resizeForOverwrite(numCon + 2, numVar + 2, stride);
+        tableau.resizeForOverwrite(numTableauRows(numCon),
+                                   numTableauCols(numVar), stride);
     }
-    // PtrMatrix<int64_t> tableau() {
-    //     return PtrMatrix<int64_t>(data.data(), numConstraints + 2,
-    //                               numVariables + 2, stride);
-    // }
     PtrMatrix<int64_t> getCostsAndConstraints() {
-        return tableau.view(1, tableau.numRow(), 2, tableau.numCol());
+        return tableau.view(numExtraRows - 1, tableau.numRow(), numExtraCols,
+                            tableau.numCol());
+    }
+    PtrMatrix<const int64_t> getCostsAndConstraints() const {
+        return tableau.view(numExtraRows - 1, tableau.numRow(), numExtraCols,
+                            tableau.numCol());
     }
     PtrMatrix<int64_t> getConstraints() {
-        return tableau.view(2, tableau.numRow(), 2, tableau.numCol());
+        return tableau.view(numExtraRows, tableau.numRow(), numExtraCols,
+                            tableau.numCol());
     }
     PtrMatrix<const int64_t> getConstraints() const {
-        return tableau.view(2, tableau.numRow(), 2, tableau.numCol());
+        return tableau.view(numExtraRows, tableau.numRow(), numExtraCols,
+                            tableau.numCol());
     }
     // note that this is 1 more than the actual number of variables
     // as it includes the constants
-    size_t getNumVar() const { return tableau.numCol() - 2; }
-    size_t getNumConstraints() const { return tableau.numRow() - 2; }
+    size_t getNumVar() const { return tableau.numCol() - numExtraCols; }
+    size_t getNumConstraints() const { return tableau.numRow() - numExtraRows; }
 
     void hermiteNormalForm() {
         inCanonicalForm = false;
@@ -70,27 +83,45 @@ struct Simplex {
             NormalForm::simplifyEqualityConstraintsImpl(getConstraints(), 1));
     }
     void deleteConstraint(size_t c) {
-        eraseConstraintImpl(tableau, c + 2);
+        eraseConstraintImpl(tableau, numTableauRows(c));
         --tableau.M;
     }
+    llvm::ArrayRef<int64_t> getTableauRow(size_t i) const {
+        return llvm::ArrayRef<int64_t>(tableau.data() + numExtraCols +
+                                           i * tableau.rowStride(),
+                                       getNumVar());
+    }
+    llvm::ArrayRef<int64_t> getBasicConstraints() const {
+        return getTableauRow(0);
+    }
+    llvm::ArrayRef<int64_t> getCost() const { return getTableauRow(1); }
     llvm::MutableArrayRef<int64_t> getTableauRow(size_t i) {
-        return llvm::MutableArrayRef<int64_t>(
-            tableau.data() + 2 + i * tableau.rowStride(), getNumVar());
+        return llvm::MutableArrayRef<int64_t>(tableau.data() + numExtraCols +
+                                                  i * tableau.rowStride(),
+                                              getNumVar());
     }
     llvm::MutableArrayRef<int64_t> getBasicConstraints() {
         return getTableauRow(0);
     }
     llvm::MutableArrayRef<int64_t> getCost() { return getTableauRow(1); }
+    StridedVector<const int64_t> getTableauCol(size_t i) const {
+        return StridedVector<const int64_t>{
+            tableau.data() + i + numExtraRows * tableau.rowStride(),
+            getNumConstraints(), tableau.rowStride()};
+    }
+    StridedVector<const int64_t> getBasicVariables() const {
+        return getTableauCol(0);
+    }
+    StridedVector<const int64_t> getConstants() const {
+        return getTableauCol(1);
+    }
     StridedVector<int64_t> getTableauCol(size_t i) {
         return StridedVector<int64_t>{tableau.data() + i +
-                                          2 * tableau.rowStride(),
+                                          numExtraRows * tableau.rowStride(),
                                       getNumConstraints(), tableau.rowStride()};
     }
     StridedVector<int64_t> getBasicVariables() { return getTableauCol(0); }
-    StridedVector<int64_t> getConstraintDenominators() {
-        return getTableauCol(1);
-    }
-    StridedVector<int64_t> getConstants() { return getTableauCol(2); }
+    StridedVector<int64_t> getConstants() { return getTableauCol(1); }
     bool initiateFeasible() {
         // remove trivially redundant constraints
         hermiteNormalForm();
@@ -99,7 +130,7 @@ struct Simplex {
         // original number of variables
         const size_t numVar = getNumVar();
         PtrMatrix<int64_t> C{getConstraints()};
-        auto basicCons = getBasicConstraints();
+        llvm::MutableArrayRef<int64_t> basicCons = getBasicConstraints();
         for (auto &&x : basicCons)
             x = -2;
         // first pass, we make sure the equalities are >= 0
@@ -158,8 +189,6 @@ struct Simplex {
             if (basicVars[i] == -1)
                 augmentVars.push_back(i);
         addVars(augmentVars.size()); // NOTE: invalidates all refs
-        for (auto &&d : getConstraintDenominators())
-            d = 1;
         {
             PtrMatrix<int64_t> C{getConstraints()};
             auto costs{getCost()};
@@ -316,6 +345,22 @@ struct Simplex {
             truncateVars(j);
         }
     }
+    static uint64_t toMask(llvm::ArrayRef<int64_t> x) {
+        assert(x.size() <= 64);
+        uint64_t m = 0;
+        for (auto y : x)
+            m = ((m << 1) | (y != 0));
+        return m;
+    }
+    uint64_t getBasicTrueVarMask() const {
+        const size_t numVarTotal = getNumVar();
+        assert(numVar <= 64);
+        uint64_t m = 0;
+        llvm::ArrayRef<int64_t> basicCons{getBasicConstraints()};
+        for (size_t i = numSlackVar; i < numVarTotal; ++i)
+            m = ((m << 1) | (basicCons[i] > 0));
+        return m;
+    }
 
     std::tuple<Simplex, IntMatrix, uint64_t> rotate(const IntMatrix &A) const {
         PtrMatrix<const int64_t> C{getConstraints()};
@@ -370,14 +415,14 @@ struct Simplex {
             // knownNonNegative[nonNegativeIndex] = true;
         }
         // all `false` indices of `knownNonNegative` indicate
-	size_t numPositive = std::popcount(knownNonNegative);
+        size_t numPositive = std::popcount(knownNonNegative);
         size_t numUnknownSign = numVar - numPositive;
         // Now, we create structure
         // C(:,0) = C(:,1:numSlackVar)*s_0 + (C(:,numSlackVar+1:end)*A(:,nn))*z
         //  + (C(:,numSlackVar+1:end)*A(:,!nn))*(y^+ - y^-)
         // C(:,0) = C(:,1:numSlackVar)*s_0 + (C(:,numSlackVar+1:end)*A)*z^*
         //  - (C(:,numSlackVar+1:end)*A(:,!nn))*y^-
-        // 0 = I * s_1 - A*z^* + A(:,!nn) * y^-
+        // 0 = I(!nn,:) * s_1 - A(!nn,:)*(y^+ - y^-)
         // where `nn` refers to the known non-negative indices
         // and we have separated `y` into `z`, `y^+`, and `y^-`.
         // z = y[nn]
@@ -386,27 +431,174 @@ struct Simplex {
         // y^- >= 0
         //
         // Layout is as follows:
-        // [s_0, z^*, s_1, y^-]
+        // [1, s_0, s_1, z^*, y^-, aug]
         // where z^* is `z` intermixed with `y^+`
         // We will proceed by trying to maximize `y^-`,
         // shifting it until we get the maximum value
         // to be `0`, in which case we can drop it and let `y^+ -> z`.
-        // once all `y^-` are gone, we can render all `s_1` non-basic,
-        // and then drop these as well.
+        // once all `y^-` are gone, can we drop `s_1`???
+        // TODO: see if this is legal, if so we probably want to separate them
         // We can then finally return the simplex as well as the shifts needed
         // for positivity.
-	// We use the full s_1 for initializing the simplex.
+        // `aug` are augments to get the simplex into canonical form.
         std::tuple<Simplex, IntMatrix, uint64_t> ret{
             {}, {numUnknownSign, numVar}, knownNonNegative};
 
         Simplex &simplex{std::get<0>(ret)};
         IntMatrix &S{std::get<1>(ret)}; // S for Shift
-	size_t numConstraintsNew = getNumConstraints() + numVar;
-	// additional variables are numUnownSign s_1s + numUnknownSign y^-s.
-	size_t numVarTotalNew = numVarTotal + numVar + numUnknownSign;
-	simplex.resizeForOverwrite(numConstraintsNew, numVarTotalNew);
-	
-	
+        const size_t numConstraintsOld = getNumConstraints();
+        // one additional constraint for each unknown sign
+        size_t numConstraintsNew = numConstraintsOld + numUnknownSign;
+        // numTrueBasic is the number of non-slack variables in the old simplex
+        // that are basic
+        // we'll add a temporary slack variable for each of these for sake of
+        // initializing the tableau in canonical form.
+        uint64_t basicTrueVarMask = getBasicTrueVarMask();
+        size_t numTrueBasic = std::popcount(basicTrueVarMask);
+        // additional variables are numUnownSign s_1s + numUnknownSign y^-s +
+        // numTrueBasic.
+        size_t numVarTotalNew = numVarTotal + numUnknownSign + numUnknownSign;
+        size_t numVarTotalNewAug = numVarTotalNew + numTrueBasic;
+        size_t s1Offset = 1 + numSlackVar;
+        size_t zStarOffset = s1Offset + numUnknownSign;
+        size_t yMinusOffset = zStarOffset + numVar;
+        simplex.numSlackVar = numSlackVar + numUnknownSign;
+        // resize instead of resizeForOverwrite because we want lots of 0s
+        // maybe we should check if resizeForOverwrite + explicitly writing them
+        // is faster
+        simplex.resize(numConstraintsNew, numVarTotalNewAug);
+        PtrMatrix<int64_t> D{simplex.getConstraints()};
+        // first block of `D` corresponds to `s_0`, and is a copy of the slack
+        // vars
+        for (size_t j = 0; j < numConstraintsOld; ++j)
+            for (size_t i = 0; i <= numSlackVar; ++i)
+                D(j, i) = C(j, i);
+        // next block of `D` is 0 (corresponding to s_1)
+        // next block is C(:,trueVars)*A
+        matmul(D.view(0, numConstraintsOld, zStarOffset, yMinusOffset),
+               C.view(0, numConstraintsOld, zStarOffset, yMinusOffset), A);
+        // then we have -C(:,trueVars)*A(:,!nn), corresponding to y^-
+        for (size_t j = 0; j < numConstraintsOld; ++j) {
+            uint64_t m = knownNonNegative;
+            size_t k = numSlackVar + 1;
+            for (size_t i = 0; i < numUnknownSign; ++i) {
+                uint64_t o = std::countr_one(m);
+                k += o;
+                m >>= ++o;
+                D(j, i + yMinusOffset) = -D(j, k++);
+            }
+        }
+        // the final block corresponds to the augments; first, we set Cons=-1
+        // so that we can also set these at the same time.
+        llvm::MutableArrayRef<int64_t> basicCons{simplex.getBasicConstraints()};
+        for (auto &&x : basicCons)
+            x = -1;
+        StridedVector<int64_t> basicVars{simplex.getBasicVariables()};
+        llvm::MutableArrayRef<int64_t> costs{simplex.getCost()};
+        if (numTrueBasic) {
+            uint64_t m = basicTrueVarMask;
+            size_t k = 0;
+            for (size_t i = 0; i < numTrueBasic; ++i) {
+                uint64_t o = std::countr_zero(m);
+                k += o;
+                m >>= ++o;
+                for (size_t j = 0; j < numVarTotalNew; ++j)
+                    costs[j] -= D(k, j);
+                size_t c = numVarTotalNew + i;
+                basicCons[c] = k;
+                basicVars[k] = c;
+                D(k++, c) = 1;
+            }
+        }
+        // now for the new constraints
+        // first block, corresponding to `s_0`; it is `0`
+        // second block corresponds to `z^*`; we have rows of `A` corresponding
+        // to unknown sign.
+        // we also handle the y^- block here.
+        {
+            uint64_t m = knownNonNegative;
+            size_t k = 0;
+            for (size_t i = 0; i < numUnknownSign; ++i) {
+                uint64_t o = std::countr_one(m);
+                k += o;
+                m >>= ++o;
+                // copy A(k,:) into D(numConstraintsOld+i, ...)
+                for (size_t j = 0; j < numVar; ++j)
+                    D(numConstraintsOld + i, j + zStarOffset) = -A(k, j);
+                size_t k2 = 0;
+                size_t m2 = knownNonNegative;
+                for (size_t j = 0; j < numUnknownSign; ++j) {
+                    uint64_t o2 = std::countr_one(m2);
+                    k2 += o2;
+                    m2 >>= ++o2;
+                    D(numConstraintsOld + i, j + yMinusOffset) = A(k, k2++);
+                }
+                ++k;
+            }
+        }
+        // now the s_1 block is `I`
+        for (size_t i = 0; i < numUnknownSign; ++i)
+            D(numConstraintsOld + i, s1Offset + i) = 1;
+        // finally, the augment block is `0`.
+
+        // now we set the initial basicVars and basicCons
+        // we already set the augments, corresponding to old constraints
+        // where true variables were basic.
+        // Now we set those corresponding to slack variables.
+        llvm::ArrayRef<int64_t> basicConsOld{getBasicConstraints()};
+        for (size_t i = 1; i <= numSlackVar; ++i) {
+            int64_t j = basicConsOld[i];
+            if (j >= 0) {
+                basicCons[i] = j;
+                basicVars[j] = i;
+            }
+        }
+        // now we set those corresponding to the new constraints.
+        // for the new constraints, it is simply `I` corresponding to `s_1`
+        for (size_t i = 0; i < numUnknownSign; ++i) {
+            basicCons[numVarTotal + i] = numConstraintsOld + i;
+            basicVars[numConstraintsOld + i] = numVarTotal + i;
+        }
+        // now, initialize costs to remove augment vars
+        if (numTrueBasic) {
+            int64_t r = simplex.runCore();
+            assert(r == 0);
+            simplex.truncateVars(numVarTotalNew);
+            // we re-zero costs so `numVarTotalNew > yMinusOffset` can
+            // assume that costs are 0
+            if (numUnknownSign)
+                for (auto &&c : costs)
+                    c = 0;
+        }
+        // now our variables are (no augment)
+        // [1, s_0, s_1, z^*, y^-]
+        // now, our goal is to eliminate `y^-`
+        if (numVarTotal) {
+            auto CC{simplex.getCostsAndConstraints()};
+            do {
+		size_t i = numVarTotalNew;
+                size_t j = zStarOffset + ((--i) - yMinusOffset);
+                costs[j] = 2;
+                costs[i] = -1;
+                int64_t c = basicCons[j];
+                if (c != -1) {
+                    NormalForm::zeroWithRowOperation(CC, 0, ++c, j);
+		    assert(basicCons[i] == -1);
+		} else {
+		    c = basicCons[i];
+		    if (c != -1)
+			NormalForm::zeroWithRowOperation(CC, 0, ++c, j);
+		}
+		simplex.run();
+		if ((basicCons[i] == -1) || (D(i,0)==0)){
+		    // i == 0
+		    numVarTotalNew = i;
+		    simplex.truncateVars(i);
+		} else {
+		    // redefine variable, add offset to `S`
+		}
+            } while (numVarTotalNew > yMinusOffset);
+        }
         return ret;
     }
 };
