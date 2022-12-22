562b3459277df7b059d137dfe722eb6b8c1c80dd
Chris Elrod <elrodc@gmail.com>
2022-12-21 19:48:40 -0500

    try to define opt for linux

--- test/remarks_test.cpp
+++ test/remarks_test.cpp
@@ -2,7 +2,6 @@
 #include <cstdio>
 #include <cstring>
 #include <string>
-// #include <gtest/gtest.h>
 
 auto main(int argc, char **argv) -> int {
   if (argc != 3)
@@ -45,22 +44,3 @@ auto main(int argc, char **argv) -> int {
     return 1005;
   return 0;
 }
-
-// // NOLINTNEXTLINE(modernize-use-trailing-return-type)
-// TEST(Remarks, BasicAssertions) {
-//   FILE *fpopt = popen("opt -mcpu=skylake-avx512 --disable-output
-//   -load-pass-plugin=./libTurboLoop.so -passes='turbo-loop'
-//   -pass-remarks-analysis='turbo-loop' ./examples/triangular_solve.ll", "r");
-//   FILE *fptxt = fopen("./examples/triangular_solve.txt", "r");
-//   std::array<char, 128> bufopt;
-//   std::array<char, 128> buftxt;
-
-//   while (fgets(bufopt.data(), sizeof(bufopt), fpopt) != nullptr) {
-//     EXPECT_NE(fgets(buftxt.data(), sizeof(buftxt), fptxt), nullptr);
-//     EXPECT_STREQ(buftxt.data(), bufopt.data());
-//   }
-//   // should be done
-//   EXPECT_EQ(fgets(buftxt.data(), sizeof(buftxt), fptxt), nullptr);
-//   EXPECT_EQ(fclose(fptxt), 0);
-//   EXPECT_EQ(pclose(fpopt), 0);
-// }
