17c9de6276659b45367260d3038ab571531d3bc6
chriselrod <elrodc@gmail.com>
2022-12-23 18:45:00 -0500

    simplify remarks_test

--- test/remarks_test.cpp
+++ test/remarks_test.cpp
@@ -1,5 +1,6 @@
 #include <array>
 #include <cstdio>
+#include <cstdlib>
 #include <cstring>
 
 auto main(int argc, char **argv) -> int {
@@ -10,46 +11,15 @@ auto main(int argc, char **argv) -> int {
   printf("modulePath: %s\n", modulePath);
   printf("examplesPath: %s\n", examplesPath);
   const char *testfile = "triangular_solve";
-  std::array<char, 512> bufopt;
+  std::array<char, 1024> bufopt;
   sprintf(
     bufopt.data(),
     "opt -mcpu=skylake-avx512 --disable-output -load-pass-plugin=%s "
-    "-passes='turbo-loop' -pass-remarks-analysis='turbo-loop' %s/%s.ll 2>&1",
-    modulePath, examplesPath, testfile);
+    "-passes=turbo-loop -pass-remarks-analysis=turbo-loop %s/%s.ll 2>&1 | "
+    "diff %s/%s.txt -",
+    modulePath, examplesPath, testfile, examplesPath, testfile);
 
-  printf("cmd: %s", bufopt.data());
-  FILE *fpopt = popen(bufopt.data(), "r");
-
-  sprintf(bufopt.data(), "%s/%s.txt", examplesPath, testfile);
-  FILE *fptxt = fopen(bufopt.data(), "r");
-
-  std::array<char, 512> buftxt;
-  int count = 0;
-  int failed = -1;
-  while (fgets(bufopt.data(), sizeof(bufopt), fpopt) != nullptr) {
-    if (fgets(buftxt.data(), sizeof(buftxt), fptxt) == nullptr)
-      return 1001;
-    if (int diff = strcmp(bufopt.data(), buftxt.data())) {
-      printf("line %d differed at %d\ntxt: %s\nopt:\n%s\n", count, diff,
-             buftxt.data(), bufopt.data());
-      failed = count;
-      break;
-    }
-    ++count;
-  }
-  if (failed >= 0) {
-    while (fgets(bufopt.data(), sizeof(bufopt), fpopt) != nullptr)
-      puts(bufopt.data());
-    return ++failed;
-  }
-  if (count < 276)
-    return 1002;
-  // should be done
-  if (fgets(buftxt.data(), sizeof(buftxt), fptxt) != nullptr)
-    return 1003;
-  if (pclose(fpopt) != 0)
-    return 1004;
-  if (fclose(fptxt) != 0)
-    return 1005;
-  return 0;
+  int rc = system(bufopt.data());
+  printf("\n\nretcode: %d\n", rc);
+  return rc != 0;
 }
