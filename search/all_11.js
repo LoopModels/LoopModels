bf48c0156b76b192fcf78df18597fdd98747e138
chriselrod <elrodc@gmail.com>
2022-12-23 08:50:28 +0000

    Deploying to gh-pages from @ JuliaSIMD/LoopModels@3a9b5c47b51e369b6366909f6211303ddc1daf0e 🚀

--- search/all_11.js
+++ search/all_11.js
@@ -3,5 +3,5 @@ var searchData=
   ['testloopfunction_178',['TestLoopFunction',['../structTestLoopFunction.html',1,'']]],
   ['transpose_179',['Transpose',['../structLinearAlgebra_1_1Transpose.html',1,'LinearAlgebra']]],
   ['turbolooppass_180',['TurboLoopPass',['../classTurboLoopPass.html',1,'']]],
-  ['typealignment_181',['typeAlignment',['../structArrayReference.html#a8d40893e36897a4d7251fc6699d93c84',1,'ArrayReference::typeAlignment()'],['../structMemoryAccess.html#a0062ff34fe54995918590f2b87fd2db2',1,'MemoryAccess::typeAlignment()']]]
+  ['typealignment_181',['typeAlignment',['../structMemoryAccess.html#a0062ff34fe54995918590f2b87fd2db2',1,'MemoryAccess']]]
 ];
