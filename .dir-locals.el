e79bec1fe1cbaf1f8a1cd887fda1eb0c9649ce14
Chris Elrod <elrodc@gmail.com>
2022-12-26 21:07:56 -0500

    build instead of bin

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,6 @@
 ;; C-x p c (project-compile) will compile the project and run tests
 ;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Project-File-Commands.html
-((nil . ((compile-command . "cmake --build bin/test && cmake --build bin/test --target test"))))
+((nil . ((compile-command . "cmake --build build/test && cmake --build build/test --target test"))))
 
 
 
