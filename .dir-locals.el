991a1be08723e9de4401be5b4c1c526a1b490ff4
Chris Elrod <elrodc@gmail.com>
2023-01-07 23:28:27 -0500

    use rtti in tests when using ubsan

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,6 @@
 ;; C-x p c (project-compile) will compile the project and run tests
 ;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Project-File-Commands.html
-((nil . ((compile-command . "cmake --build buildgcc/test && CTEST_OUTPUT_ON_FAILURE=1 cmake --build buildgcc/test --target test && gcovr --coveralls coverage-final.json && cmake --build buildclang/test && CTEST_OUTPUT_ON_FAILURE=1 cmake --build buildclang/test --target test"))))
+((nil . ((compile-command . "UBSAN_OPTIONS='print_stacktrace=1' cmake --build buildgcc/test && UBSAN_OPTIONS='print_stacktrace=1' CTEST_OUTPUT_ON_FAILURE=1 cmake --build buildgcc/test --target test && gcovr --coveralls coverage-final.json && UBSAN_OPTIONS='print_stacktrace=1' cmake --build buildclang/test && UBSAN_OPTIONS='print_stacktrace=1' CTEST_OUTPUT_ON_FAILURE=1 cmake --build buildclang/test --target test"))))
 
 
 
