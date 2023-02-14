d0d5d5e800acb4d68bfbb2fdf65c8e0f9b62a119
chriselrod <elrodc@gmail.com>
2023-02-14 16:07:20 +0000

    Deploying to gh-pages from @ JuliaSIMD/LoopModels@4329691c2065ac09a714ef3d419f6c5dfbdd9874 🚀

--- search/classes_3.js
+++ search/classes_3.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['default_5fcapacity_5ftype_214',['default_capacity_type',['../structLinearAlgebra_1_1default__capacity__type.html',1,'LinearAlgebra']]],
-  ['default_5fcapacity_5ftype_3c_20t_20_3e_215',['default_capacity_type&lt; T &gt;',['../structLinearAlgebra_1_1default__capacity__type_3_01T_01_4.html',1,'LinearAlgebra']]],
+  ['default_5fcapacity_5ftype_3c_20s_20_3e_215',['default_capacity_type&lt; S &gt;',['../structLinearAlgebra_1_1default__capacity__type_3_01S_01_4.html',1,'LinearAlgebra']]],
   ['densedims_216',['DenseDims',['../structLinearAlgebra_1_1DenseDims.html',1,'LinearAlgebra']]],
   ['densemapinfo_3c_20instruction_3a_3aintrinsic_2c_20void_20_3e_217',['DenseMapInfo&lt; Instruction::Intrinsic, void &gt;',['../structllvm_1_1DenseMapInfo_3_01Instruction_1_1Intrinsic_00_01void_01_4.html',1,'llvm']]],
   ['densemapinfo_3c_20llvm_3a_3amutablearrayref_3c_20t_20_3e_20_3e_218',['DenseMapInfo&lt; llvm::MutableArrayRef&lt; T &gt; &gt;',['../structllvm_1_1DenseMapInfo_3_01llvm_1_1MutableArrayRef_3_01T_01_4_01_4.html',1,'llvm']]],
