35700ae18c9b0ce4de2fbb87846d13dc90a5a40e
Chris Elrod <elrodc@gmail.com>
2023-03-15 20:36:14 -0400

    Support countDigits for smaller integer types

--- include/Math/Math.hpp
+++ include/Math/Math.hpp
@@ -426,10 +426,13 @@ template <std::unsigned_integral T> constexpr auto countDigits(T x) {
             6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9,  10, 10, 10, 10,
             11, 11, 11, 12, 12, 12, 13, 13, 13, 13, 14, 14, 14, 15, 15, 15, 16,
             16, 16, 16, 17, 17, 17, 18, 18, 18, 19, 19, 19, 19, 20};
-  } else {
-    static_assert(sizeof(T) == 4);
+  } else if constexpr (sizeof(T) == 4) {
     bits = {1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4,  5,  5, 5,
             6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 9, 9, 9, 10, 10, 10};
+  } else if constexpr (sizeof(T) == 2) {
+    bits = {1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5};
+  } else if constexpr (sizeof(T) == 1) {
+    bits = {1, 1, 1, 1, 2, 2, 2, 3, 3};
   }
   T digits = bits[8 * sizeof(T) - std::countl_zero(x)];
   return digits - (x < powers[digits - 1]);
