3984468ba2c132a470aba2681017d26f25ffc538
chriselrod <elrodc@gmail.com>
2023-03-16 23:47:08 +0000

    Deploying to gh-pages from @ JuliaSIMD/LoopModels@a5fd9aef4069374915365f8830cf6f406dc1827b 🚀

--- search/all_11.js
+++ search/all_11.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['tableau_192',['Tableau',['../structTableau.html',1,'']]],
-  ['testloopfunction_193',['TestLoopFunction',['../structTestLoopFunction.html',1,'']]],
+  ['testloopfunction_193',['TestLoopFunction',['../classTestLoopFunction.html',1,'']]],
   ['transpose_194',['Transpose',['../structLinAlg_1_1Transpose.html',1,'LinAlg']]],
   ['turbolooppass_195',['TurboLoopPass',['../classTurboLoopPass.html',1,'']]],
   ['typealignment_196',['typeAlignment',['../structMemoryAccess.html#a0062ff34fe54995918590f2b87fd2db2',1,'MemoryAccess']]]
--- search/classes_f.js
+++ search/classes_f.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['tableau_345',['Tableau',['../structTableau.html',1,'']]],
-  ['testloopfunction_346',['TestLoopFunction',['../structTestLoopFunction.html',1,'']]],
+  ['testloopfunction_346',['TestLoopFunction',['../classTestLoopFunction.html',1,'']]],
   ['transpose_347',['Transpose',['../structLinAlg_1_1Transpose.html',1,'LinAlg']]],
   ['turbolooppass_348',['TurboLoopPass',['../classTurboLoopPass.html',1,'']]]
 ];
