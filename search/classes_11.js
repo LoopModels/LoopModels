0ac6db8bd7bc353f398a05f356a5e98b59a8db37
chriselrod <elrodc@gmail.com>
2023-03-31 04:11:30 +0000

    Deploying to gh-pages from @ JuliaSIMD/LoopModels@01cae2c94dd698f1b3bf64a22589ccbab4797c72 🚀

--- search/classes_11.js
+++ search/classes_11.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['uniformscaling_384',['UniformScaling',['../structLinAlg_1_1UniformScaling.html',1,'LinAlg']]],
-  ['urand_385',['urand',['../structurand.html',1,'']]],
-  ['urand_3c_20double_20_3e_386',['urand&lt; double &gt;',['../structurand_3_01double_01_4.html',1,'']]]
+  ['urand_385',['URand',['../structURand.html',1,'']]],
+  ['urand_3c_20double_20_3e_386',['URand&lt; double &gt;',['../structURand_3_01double_01_4.html',1,'']]]
 ];
