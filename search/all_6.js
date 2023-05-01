1abd5ff3e3e5ddca815ff713b37220fb270efb7c
chriselrod <elrodc@gmail.com>
2023-05-01 07:01:09 +0000

    Deploying to gh-pages from @ JuliaSIMD/LoopModels@d9df5acf700e323d7a61d0f39b7c35ccb60234f3 🚀

--- search/all_6.js
+++ search/all_6.js
@@ -4,7 +4,7 @@ var searchData=
   ['geteltype_83',['GetEltype',['../structGetEltype.html',1,'']]],
   ['geteltype_3c_20a_20_3e_84',['GetEltype&lt; A &gt;',['../structGetEltype_3_01A_01_4.html',1,'']]],
   ['geteltype_3c_20rational_20_3e_85',['GetEltype&lt; Rational &gt;',['../structGetEltype_3_01Rational_01_4.html',1,'']]],
-  ['getfusionomega_86',['getFusionOmega',['../structMemoryAccess.html#a343e8ad8af62a3d1303005ad852c1d39',1,'MemoryAccess::getFusionOmega() -&gt; MutPtrVector&lt; int64_t &gt;'],['../structMemoryAccess.html#a685e16ba012a1a0373eeb6c1d21a5666',1,'MemoryAccess::getFusionOmega() const -&gt; PtrVector&lt; int64_t &gt;']]],
+  ['getfusionomega_86',['getFusionOmega',['../structMemoryAccess.html#ae0990dc73d09b0b1bcd5bff09998cf52',1,'MemoryAccess::getFusionOmega() -&gt; MutPtrVector&lt; int64_t &gt;'],['../structMemoryAccess.html#a83187a91201e29b64d0f3cb40b8029f1',1,'MemoryAccess::getFusionOmega() const -&gt; PtrVector&lt; int64_t &gt;']]],
   ['getinindmat_87',['getInIndMat',['../classDependence.html#a6cc677c17009dae397e882a80eb727a4',1,'Dependence']]],
   ['getinstruction_88',['getInstruction',['../structInstruction_1_1Cache.html#abe269ee408a3bb0ae54cc6989fa64d5c',1,'Instruction::Cache']]],
   ['getmaxdigits_89',['getMaxDigits',['../namespaceLinAlg.html#a159c56bc82c7e302c1228e5148038ada',1,'LinAlg::getMaxDigits(PtrMatrix&lt; Rational &gt; A) -&gt; Vector&lt; size_t &gt;'],['../namespaceLinAlg.html#a636fee1b1a5930a3c38e46eedbd57c67',1,'LinAlg::getMaxDigits(PtrMatrix&lt; T &gt; A) -&gt; Vector&lt; T &gt;']]],
--- search/functions_5.js
+++ search/functions_5.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['getconflict_426',['getConflict',['../structPredicate_1_1Set.html#af27ae955d06bb795f220e9f19c602669',1,'Predicate::Set']]],
-  ['getfusionomega_427',['getFusionOmega',['../structMemoryAccess.html#a343e8ad8af62a3d1303005ad852c1d39',1,'MemoryAccess::getFusionOmega() -&gt; MutPtrVector&lt; int64_t &gt;'],['../structMemoryAccess.html#a685e16ba012a1a0373eeb6c1d21a5666',1,'MemoryAccess::getFusionOmega() const -&gt; PtrVector&lt; int64_t &gt;']]],
+  ['getfusionomega_427',['getFusionOmega',['../structMemoryAccess.html#ae0990dc73d09b0b1bcd5bff09998cf52',1,'MemoryAccess::getFusionOmega() -&gt; MutPtrVector&lt; int64_t &gt;'],['../structMemoryAccess.html#a83187a91201e29b64d0f3cb40b8029f1',1,'MemoryAccess::getFusionOmega() const -&gt; PtrVector&lt; int64_t &gt;']]],
   ['getinindmat_428',['getInIndMat',['../classDependence.html#a6cc677c17009dae397e882a80eb727a4',1,'Dependence']]],
   ['getinstruction_429',['getInstruction',['../structInstruction_1_1Cache.html#abe269ee408a3bb0ae54cc6989fa64d5c',1,'Instruction::Cache']]],
   ['getmaxdigits_430',['getMaxDigits',['../namespaceLinAlg.html#a159c56bc82c7e302c1228e5148038ada',1,'LinAlg::getMaxDigits(PtrMatrix&lt; Rational &gt; A) -&gt; Vector&lt; size_t &gt;'],['../namespaceLinAlg.html#a636fee1b1a5930a3c38e46eedbd57c67',1,'LinAlg::getMaxDigits(PtrMatrix&lt; T &gt; A) -&gt; Vector&lt; T &gt;']]],
