11a1b5a866dce641dec3d2b621b4bbd0986fd421
chriselrod <elrodc@gmail.com>
2023-05-12 22:46:01 +0000

    Deploying to gh-pages from @ JuliaSIMD/LoopModels@07b7fafc7363c284596b4a5e286e28a5e4d1db4c 🚀

--- search/functions_b.js
+++ search/functions_b.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['searchoperandsforloads_464',['searchOperandsForLoads',['../classLinearProgramLoopBlock.html#a3fa61f7357b11b5fdc6ce77cfeadcd94',1,'LinearProgramLoopBlock']]],
-  ['searchvalueforstores_465',['searchValueForStores',['../classLinearProgramLoopBlock.html#a720cbd2c565fd414966632b6408bab11',1,'LinearProgramLoopBlock']]],
+  ['searchoperandsforloads_464',['searchOperandsForLoads',['../classLinearProgramLoopBlock.html#aba9f909e08e1c2088f72883b3dafdaae',1,'LinearProgramLoopBlock']]],
+  ['searchvalueforstores_465',['searchValueForStores',['../classLinearProgramLoopBlock.html#a06f63fe9b66d7b330be607bdba3f1d71',1,'LinearProgramLoopBlock']]],
   ['selectcost_466',['selectCost',['../structInstruction.html#a5f1f568a7c35b55c492522e3f70e0ed4',1,'Instruction']]]
 ];
