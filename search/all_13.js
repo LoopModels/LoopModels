7e69293364404f66459105cd32ce146808084117
chriselrod <elrodc@gmail.com>
2023-05-26 03:50:17 +0000

    Deploying to gh-pages from @ JuliaSIMD/LoopModels@50ce1ed34172984286297f841670846930849359 🚀

--- search/all_13.js
+++ search/all_13.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['val_235',['Val',['../structVal.html',1,'']]],
+  ['val_235',['Val',['../classVal.html',1,'']]],
   ['vectorprinter_236',['VectorPrinter',['../classprettyprinters_1_1VectorPrinter.html',1,'prettyprinters']]]
 ];
