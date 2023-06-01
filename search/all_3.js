c96edc8a748fbeb27952e04cceae2d5541dbcf70
chriselrod <elrodc@gmail.com>
2023-06-01 04:23:20 +0000

    Deploying to gh-pages from @ JuliaSIMD/LoopModels@676003c6a4c34251fe88e68c59d27fcb9954ce97 🚀

--- search/all_3.js
+++ search/all_3.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['default_5fcapacity_5ftype_58',['default_capacity_type',['../structLinAlg_1_1default__capacity__type.html',1,'LinAlg']]],
-  ['default_5fcapacity_5ftype_3c_20s_20_3e_59',['default_capacity_type&lt; S &gt;',['../structLinAlg_1_1default__capacity__type_3_01S_01_4.html',1,'LinAlg']]],
+  ['defaultcapacitytype_58',['DefaultCapacityType',['../structLinAlg_1_1DefaultCapacityType.html',1,'LinAlg']]],
+  ['defaultcapacitytype_3c_20s_20_3e_59',['DefaultCapacityType&lt; S &gt;',['../structLinAlg_1_1DefaultCapacityType_3_01S_01_4.html',1,'LinAlg']]],
   ['densedims_60',['DenseDims',['../structLinAlg_1_1DenseDims.html',1,'LinAlg']]],
   ['densematrixprinter_61',['DenseMatrixPrinter',['../classprettyprinters_1_1DenseMatrixPrinter.html',1,'prettyprinters']]],
   ['dependence_62',['Dependence',['../classDependence.html',1,'']]],
