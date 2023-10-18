ac327bd34b0afed5eb96c2cda5686af662f9d5f8
chriselrod <elrodc@gmail.com>
2023-10-18 03:08:10 -0400

    add cost function, and the dict test file I forgot to add yesterday

--- include/IR/CostFunction.hpp
+++ include/IR/CostFunction.hpp
@@ -1,12 +1,88 @@
 #pragma once
 
 #include <Math/Array.hpp>
+#include <Math/Exp.hpp>
 #include <Math/Math.hpp>
 #include <Math/Vector.hpp>
-
 namespace poly::CostModeling {
-using math::AbstractVector;
-using math::DensePtrMatrix;
+using math::AbstractVector, math::DensePtrMatrix;
+using utils::Optional;
+
+// Order is outermost -> innermost
+// Costs are relative to Scalar, i.e. scalar == 1
+struct MemoryCosts {
+  unsigned contiguous;    // vload/vstore
+  unsigned discontiguous; // gather/scatter
+};
+struct VectorizationFactor {
+  unsigned l2factor;
+  unsigned index; // outermost == 0
+};
+
+struct OrthogonalAxes {
+  uint32_t data;
+  [[nodiscard]] constexpr auto contigAxis() const -> uint32_t {
+    return data & 0xff;
+  }
+  // mask containing `0` for dependent axes, 1s for independent
+  // should contain `0` for all non-existent loops, e.g.
+  // for (i = I, j = J, k = K, l = L) {
+  //   A[j,l]
+  //   for (a = A, b = B){ .... }
+  // }
+  // The mask should equal (1<<0) | (1<<2)  (for the i and k).
+  // Only loops it is nested in that it doesn't depend on count.
+  [[nodiscard]] constexpr auto indepAxes() const -> uint32_t {
+    return data >> 8;
+  };
+};
+// costs is an array of length two.
+// memory costs, unnormalized by `prod(unrolls)`
+constexpr auto cost(MemoryCosts mc, AbstractVector auto invunrolls,
+                    VectorizationFactor vfi, OrthogonalAxes orth)
+  -> utils::eltype_t<decltype(invunrolls)> {
+
+  utils::eltype_t<decltype(invunrolls)> c;
+  uint32_t da = orth.indepAxes();
+  if (da) {
+    uint32_t tz = std::countr_zero(da);
+    c = invunrolls[tz++];
+    for (uint32_t d = da >> tz, i = tz; d; d >>= tz, i += tz) {
+      tz = std::countr_zero(d);
+      c *= invunrolls[i + tz++];
+    }
+  } else c = 1;
+
+  if ((vfi.index < 32) && !(da & (1 << vfi.index))) {
+    // depends vectorized index
+    if (vfi.index == orth.contigAxis()) {
+      c *= mc.contiguous;
+    } else if (orth.contigAxis() >= 32) {
+      c *= mc.discontiguous;
+    } else {
+      // Discontiguous vector load.
+      // We consider two alternatives:
+      // 1. gather/scatter (discontiguous)
+      // 2. contiguous load for each vectorization factor of length equal to
+      // unroll, followed by shuffles.
+      // E.g., unroll contig by 4, another dim is vectorized by 8:
+      // we'd have 8 vloads (max(4/8,1) * 8), followed by 4*log2(8) +
+      // log2(max(8/4,1))*4 shuffles.
+      // Or, if we unroll contig by 8, and another dim is vectorzeed by 2, we'd
+      // have 8 = (max(8/2,1) * 2) vloads, 8*log2(2) + log2(max(2/8,1))*8
+      // shuffles.
+      // We divide by `u[contig]`, as it is now accounted for
+      // So we have
+      // max(v/u, 1) + u*log2(v) + log2(max(v/u ,1))*u
+      utils::eltype_t<decltype(invunrolls)> iu{invunrolls[orth.contigAxis()]},
+        u{1 / iu}, mr{math::smax((1 << vfi.l2factor) * iu, 1)};
+
+      c *= math::smin(mc.contiguous * mr + u * (vfi.l2factor + log2(mr)),
+                      mc.discontiguous);
+    }
+  }
+  return c;
+}
 
 /// Here, we define a cost fn that can be optimized to produce
 /// vectorization and unrolling factors.
@@ -22,19 +98,18 @@ using math::DensePtrMatrix;
 /// it depends on, divided by the product of all unroll factors of all
 /// containing loops.
 /// To optimize, we can branch and bound. Unrolling factors lead to a natural
-/// relaxation that plays well, but less so for binary variables like which loop
-/// is vectorized. Additionally, patterns such as replacing gather/scatters with
-/// shuffle sequences need special handling, that restricts the branch and bound
-/// to powers of 2. To be able to build such a cost model, we need to estimate
-/// the number of live variables as a result of unroll factors, in order to
-/// impose constraints.
+/// relaxation that plays well, but less so for binary variables like which
+/// loop is vectorized. Additionally, patterns such as replacing
+/// gather/scatters with shuffle sequences need special handling, that
+/// restricts the branch and bound to powers of 2. To be able to build such a
+/// cost model, we need to estimate the number of live variables as a result
+/// of unroll factors, in order to impose constraints.
 ///
 /// We use soft constraints for register pressuring, representing the
 /// store/reload pair of a spill.
 ///
-/// Furthermore, we also need to consider the possibility of dependency chains.
-/// Consider, for example
-/// for (ptrdiff_t i = 0; i < I; ++i){
+/// Furthermore, we also need to consider the possibility of dependency
+/// chains. Consider, for example for (ptrdiff_t i = 0; i < I; ++i){
 ///   eltype_t<A> xi = x[i];
 ///   for (ptrdiff_t j = 0; j < J; ++j)
 ///     xi += A[i][j] * y[j];
@@ -48,14 +123,14 @@ using math::DensePtrMatrix;
 /// The second option is better, but may not be possible, e.g. if there is no
 /// `i` loop or it carries some dependency. Thus, we want our model to unroll
 /// `i` when legal, and unroll `j` otherwise.
-/// Assuming a throughput of 2 fma/cycle and a latency of 4 cycles, an estimate
-/// of the cost as a function of I, J, Ui, and Uj is (ignoring vectorization):
-/// 4*I*J/min(Ui*Uj, 2*4) + 4*I*log2(Uj)
-/// The first term is latency per fma (because of the dependency chain) * the
-/// number of iterations, divided by however many unrolling allows us to have
-/// inflight. The second term is for the reduction of the cloned `Uj`
-/// accumulators. Each step in the reduction has a latency of 4 cycles, and we
-/// need to do `log2(Uj)` steps.
+/// Assuming a throughput of 2 fma/cycle and a latency of 4 cycles, an
+/// estimate of the cost as a function of I, J, Ui, and Uj is (ignoring
+/// vectorization): 4*I*J/min(Ui*Uj, 2*4) + 4*I*log2(Uj) The first term is
+/// latency per fma (because of the dependency chain) * the number of
+/// iterations, divided by however many unrolling allows us to have inflight.
+/// The second term is for the reduction of the cloned `Uj` accumulators. Each
+/// step in the reduction has a latency of 4 cycles, and we need to do
+/// `log2(Uj)` steps.
 ///
 /// Note, `y-softplus(l*(y-x))/l` is a good smooth minimum function,
 /// monotonic in `x` and differentiable everywhere. `l` controls
@@ -64,18 +139,19 @@ using math::DensePtrMatrix;
 /// Thus, a cost function for the above gemv could be something like
 /// memcost = I*J*(Ui*Uj*C_{Al} + Uj*C_{yl}) / (Ui*Uj) +
 ///    I*(C_{xl}*Ui + C_{xs}*Ui) / Ui
-/// cthroughput = I*J*(Ui*Uj*C_{t,fma}) / (Ui*Uj) + I*(Ui*C_{t,add}*(Uj-1)) / Ui
-/// clatency = I*J*C_{l,fma}/smin(Ui*Uj, C_{l,fma}/C_{t,fma}) +
+/// cthroughput = I*J*(Ui*Uj*C_{t,fma}) / (Ui*Uj) + I*(Ui*C_{t,add}*(Uj-1)) /
+/// Ui clatency = I*J*C_{l,fma}/smin(Ui*Uj, C_{l,fma}/C_{t,fma}) +
 ///    I*C_{l,add}*log2(Uj)
 /// cost = memcost + smax(cthroughput, clatency)
 /// or, if the it is easier to solve:
 /// cost = memcost + cthroughput + clatency
 ///
-/// We may initially want to add a small cost for loop increment and cmp/branch,
-/// to encourage unrolling more generally, plus a cost for unrolling to
-/// discourse any excess unrolling when it doesn't provide meaningful benefits
-/// (representing the general cost of code size/ filling uop cache -- we
-/// definitely want loops to fit in the uop cache of any CPU sporting one!!! ).
+/// We may initially want to add a small cost for loop increment and
+/// cmp/branch, to encourage unrolling more generally, plus a cost for
+/// unrolling to discourse any excess unrolling when it doesn't provide
+/// meaningful benefits (representing the general cost of code size/ filling
+/// uop cache -- we definitely want loops to fit in the uop cache of any CPU
+/// sporting one!!! ).
 ///
 ///
 ///
@@ -88,18 +164,18 @@ using math::DensePtrMatrix;
 /// then unrolling the `i` loop doesn't increase OOO,
 /// but we can assume that as successive `j` iterations are independent/do not
 /// have a dependency chain, this isn't an issue.
-/// That is, we only consider reductions across the inner-most loop as requiring
-/// cloning of accumulators.
+/// That is, we only consider reductions across the inner-most loop as
+/// requiring cloning of accumulators.
 ///
-/// On throughput modeling, LLVM seems to generally give a recip throughput of 1
-/// for pipelined instructions, regardless of number of ports. This is actually
-/// what we want, as this allows RTs to be additive (e.g., we may have a fma
-/// that is able to run on 2 ports (e.g. p0 or p5) and a permute that can only
-/// execute on one (e.g. p5); when mixing these instructions, they have the same
-/// effective cost -- they use a port -- and the more limited port choices of
-/// one isn't a problem so long as others can use what remains. For our
-/// purposes, it isn't worth getting too fancy here. It is worth noting that the
-/// baseline model presented here
+/// On throughput modeling, LLVM seems to generally give a recip throughput of
+/// 1 for pipelined instructions, regardless of number of ports. This is
+/// actually what we want, as this allows RTs to be additive (e.g., we may
+/// have a fma that is able to run on 2 ports (e.g. p0 or p5) and a permute
+/// that can only execute on one (e.g. p5); when mixing these instructions,
+/// they have the same effective cost -- they use a port -- and the more
+/// limited port choices of one isn't a problem so long as others can use what
+/// remains. For our purposes, it isn't worth getting too fancy here. It is
+/// worth noting that the baseline model presented here
 /// https://arxiv.org/pdf/2107.14210.pdf
 /// performed respectively well when compared to vastly more sophisticated
 /// tools; for example, it performed similarly well as llvm-mca on most tested
@@ -118,8 +194,8 @@ using math::DensePtrMatrix;
 /// newer, 1 for older) Unfortunately, we cannot get the CPU-specific
 /// information (`i`,`m`,or`w`) from LLVM. However, these are largely a matter
 /// of scale, and are generally correlated. E.g., Intel's Alderlake's values
-/// would be 6, 3, and 2, vs the older Skylake's 4, 2, and 1. While not all the
-/// ratios are equal (`w`'s is 2 instead of 1.5), it is unlikely that many
+/// would be 6, 3, and 2, vs the older Skylake's 4, 2, and 1. While not all
+/// the ratios are equal (`w`'s is 2 instead of 1.5), it is unlikely that many
 /// optimization decisions are going to be made differently between them.
 /// A possible exception is that we may wish to unroll more for CPUs with more
 /// out of order execution abilities. `getMaxInterleaveFactor` is an indicator
@@ -132,38 +208,38 @@ using math::DensePtrMatrix;
 /// as the cost, where `a_g = abs(a/gcd(a,b))` and `b_g = abs(b/gcd(a,b))`.
 ///
 ///
-/// Given `x[a*i + b*j + c*k]`, where neither `i` or `j` are vectorized (and
-/// `a`, `b`, and `c` are compile time constants), we use:
-/// (a_g*U_i + b_g*U_j + c_g*U_k - a_g*b_g - a_g*c_g - b_g*c_g + a_g*b_g*c_g) /
-/// (U_i * U_j * U_k)
-/// as the cost, where the `_g`s are same as before, but using the gcd of all
-/// three `a`, `b`, and `c`.
-/// NOTE: these, unlike the 2-case, are wrong, but they happen to be correct
-/// when the gcds are all `1`. It hopefully at least dominates the actual cost.
-/// Eventually, it'd be great to actually derive a general formula.
+/// Given `x[a*i + b*j + c*k]`, where none of `i`, `j`, or `k` are vectorized
+/// (and `a`, `b`, and `c` are compile time constants), we use: (a_g*U_i +
+/// b_g*U_j + c_g*U_k - a_g*b_g - a_g*c_g - b_g*c_g + a_g*b_g*c_g) / (U_i *
+/// U_j * U_k) as the cost, where the `_g`s are same as before, but using the
+/// gcd of all three `a`, `b`, and `c`. NOTE: these, unlike the 2-case, are
+/// wrong, but they happen to be correct when the gcds are all `1`. It
+/// hopefully at least dominates the actual cost. Eventually, it'd be great to
+/// actually derive a general formula.
 ///
 /// For register consumption, we
 /// 1. Determine an ordering of unroll factors for each inner most loop.
 /// 2. Define a registers used as a function of these unroll factors.
 ///
 /// Loads from inner unrolls that don't depend on any outer-unrolls must have
-/// lifetimes spanning all outer-unrolls, if they're re-used by an op depending
-/// on that outer.
-/// Our heuristic for ordering unrolls is based on the twin observations:
+/// lifetimes spanning all outer-unrolls, if they're re-used by an op
+/// depending on that outer. Our heuristic for ordering unrolls is based on
+/// the twin observations:
 /// 1. Inner unrolls are likely to consume more registers for longer.
 /// 2. More ops with overlapping lifetimes dependent on one particular loop
 /// require more registers.
 ///
-/// As the ordering of unrolls influences register pressure, we sort them first
-/// by register cost per unroll (placing those with the highest register cost
-/// outside), and then by memory op cost within these categories, placing the
-/// highest costs innermost  (higher memory cost means lower unroll relative to
-/// the lower cost, so that we get more reuse on the higher cost operations;
-/// lower unroll means we place inside, reducing the cost of these unrolls).
+/// As the ordering of unrolls influences register pressure, we sort them
+/// first by register cost per unroll (placing those with the highest register
+/// cost outside), and then by memory op cost within these categories, placing
+/// the highest costs innermost  (higher memory cost means lower unroll
+/// relative to the lower cost, so that we get more reuse on the higher cost
+/// operations; lower unroll means we place inside, reducing the cost of these
+/// unrolls).
 ///
-/// So, how do we define register cost per unroll in an unroll-order independent
-/// manner, so that we can use this for determining the order?
-/// for (int m=0; m<M; ++m){
+/// So, how do we define register cost per unroll in an unroll-order
+/// independent manner, so that we can use this for determining the order? for
+/// (int m=0; m<M; ++m){
 ///   for (int n=0; n<N; ++n){
 ///     auto Cmn = C[m,n];
 ///     for (int k=0; k<K; ++k)
